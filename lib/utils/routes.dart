2b3c89c1a7115029648a8a260495b357569e6cbe
ThenuraDissanayake <120773622+ThenuraDissanayake@users.noreply.github.com>
2024-04-11 21:07:35 +0530

    Delete lib directory

--- lib/utils/routes.dart
+++
@@ -1,9 +0,0 @@
-class MyRoutes {
-  static String welcomeRoute = "/welcome";
-  static String registrationRoute = "/registration";
-  static String loginRoute = "/login";
-  static String dashboardRoute = "/dashboard";
-  static String itemRoute = "/items";
-  static String itemDetailRoute = "/item_detail";
-  static String cartRoute = "/cart";
-}
